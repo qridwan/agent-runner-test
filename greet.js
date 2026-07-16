33fc7e4f32d33047b62aafaa802539b473abf08f
qridwan <404ridwan@gmail.com>
2026-07-16 22:50:01 +0600

    Add farewell function and export alongside greet

--- greet.js
+++ greet.js
@@ -1,4 +1,9 @@
 function greet(name) {
   return "Hello, " + name;
 }
-module.exports = greet;
+
+function farewell(name) {
+  return "Goodbye, " + name + "!";
+}
+
+module.exports = { greet, farewell };
